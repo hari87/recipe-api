892055b826d415c0605d0db4022270417efa4f55
Hariprasad Ramesh Rao <hariprasad.ramesh.rao@signify.com>
2021-04-25 12:32:52 +0200

    adding typescript object builder plugin

---
+++ typescript/recipe-api.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+/* eslint-disable */
+// Generated using typescript-generator version 2.24.612 on 2021-04-25 09:20:24.
+
+export interface RecipeInfo {
+    creationDate: Date;
+    isVegetarian: boolean;
+    meantForHowManyPeople: number;
+    ingredients: string[];
+    cookingInstructions: string;
+    recipeName: string;
+}
+
+export interface DefaultSuccess {
+    status: string;
+}
+
+export interface ErrorMessage {
+    errorCode: number;
+    errorDescription: string;
+}
